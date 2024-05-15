36f30612c5ad4ebfa931246ff0f87966948454a4
KaviprakashRamalingam <ramalingam.ka@northeastern.edu>
2024-05-15 17:42:40 -0400

    Create CdkApplicationTests.java

---
+++ src/test/java/com/company/cdk/CdkApplicationTests.java
@@ -0,0 +1,14 @@
+package com.company.cdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdkApplicationTests {
+	
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
